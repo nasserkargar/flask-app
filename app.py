304ae7b3f820ec4dcdb09132f5aa59af9ab658e7
cloud <cloud@example.com>
2022-07-18 14:41:03 +0000

    initial version

---
+++ app.py
@@ -0,0 +1,10 @@
+import redis
+from flask import Flask
+app=Flask(__name__)
+cache=redis.Redis(host='redis',port=6379)
+def get_hit_count():
+	return cache.incr('hits')
+@app.route('/')
+def hello():
+	count=get_hit_count()
+	return 'Hello Iran {}'.format(count)
